e63e3e86feacdd69f43c1d601087bd65470c1a25
Huertz <104065687+Huertz@users.noreply.github.com>
2024-01-06 16:36:17 -0600

    removed unecessary lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import CssBaseline from "@mui/material/CssBaseline";
-import "./App.css";
-import TodoList from "./TodoList";
-import Navbar from "./Navbar";
+import CssBaseline from '@mui/material/CssBaseline';
+import './App.css';
+import TodoList from './TodoList';
+import Navbar from './Navbar';
 
 function App() {
   return (
